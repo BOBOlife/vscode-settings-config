099cfd2e70b8e6e9ac6334337a435b97fcf35b4c
BoWang <leo@192.168.0.103>
2024-02-13 20:19:10 +0800

    update: 新增gitlen配置 & Search.exclude配置 & 文档更新说明

---
+++ README.md
@@ -0,0 +1,38 @@
+# 介绍
+
+这是一个个人的 vscode settings
+
+# extensions
+
+我这是我所用的插件
+
+- Carbon Product Icons
+- Catppuccin Icons for Vscode
+- Code Spell Checker
+- Dark Molokai Theme
+- Error Lens
+- ESLint
+- File Nesting Updater
+- file-icons
+- Git Graph
+- GitLens
+- Gruvbox Material
+- Iconify ItelliSense
+- Import Cost
+- Moegi Theme
+- Monokai Charcoal high contrast
+- Path Itellisense
+- Prettier
+- Project Manager
+- TSConfig Helper
+- Volar
+- Vim
+- UnoCSS
+- vscode-pigments
+- Where Am I
+
+# vim
+
+我是一个 vim 键盘侠 ， 此插件开发的出发点就是为了自己的 vim 的配置。
+
+如果你也是个 vscode 的 vim 使用者那么 你也可以试试用我的配置来体验一下。
--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,25 @@
 import type { ExtensionContext } from "vscode";
 import { commands, workspace, ConfigurationTarget, window } from "vscode";
 import { vimConfig } from "./vimSettings";
+import { gitLenConfig } from "./gitLen";
+import { searchExcludeConfig } from "./search.exclude";
 
 export function activate(context: ExtensionContext) {
   console.log(
-    'Congratulations, your extension "vscode-vim-config" is now active!'
+    'Congratulations, your extension "vscode-settings-config" is now active!'
   );
 
   let disposable = commands.registerCommand(
     "vscode-settings-config.configActivate",
     () => {
       const config = workspace.getConfiguration();
-      const vimSettings = {
+      const vscodeSettings = {
         ...vimConfig,
+        ...gitLenConfig,
+        ...searchExcludeConfig,
       };
 
-      for (const [key, value] of Object.entries(vimSettings)) {
+      for (const [key, value] of Object.entries(vscodeSettings)) {
         config.update(key, value, ConfigurationTarget.Global).then(() => {
           window.showInformationMessage(`Vim setting ${key} has been updated.`);
         });
